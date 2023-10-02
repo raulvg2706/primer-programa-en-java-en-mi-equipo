41107da45bf083f290f8ad17391fad4d5f91d7aa
Raúl Valbuena García <valbuenagarciaraul@gmail.com>
2023-10-02 09:11:42 +0200

    Este programa muestra mis apellidos, el año en que naci y mi edad

--- prueba1.java
+++ prueba1.java
@@ -3,7 +3,7 @@ public class prueba1{
 	public static void main(String args[]) {
 
 		System.out.println("Valbuena García");
-		System.out.println("Nací en 2004 y tengo 19 años")
+		System.out.println("Nací en 2004 y tengo 19 años");
 	} 
 
 }
