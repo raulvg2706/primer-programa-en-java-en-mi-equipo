f960a63b8e8946b56a31c0b09245895f4e33ab9b
Raúl Valbuena García <valbuenagarciaraul@gmail.com>
2023-10-02 09:03:00 +0200

    Este programa muestra mis apellidos

--- prueba1.java
+++ prueba1.java
@@ -4,7 +4,6 @@ public class prueba1{
 
 		System.out.println("Valbuena García");
 		System.out.println("Tengo 19 años y naci en 2004");
-		System.out.println("Mi comida favorita es el arroz con pollo");
 	} 
 
 }
