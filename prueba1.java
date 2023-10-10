49bc4994c4c5520ab86b4bf934dff98ef131eb5c
Raúl Valbuena García <valbuenagarciaraul@gmail.com>
2023-10-10 12:53:47 +0200

    Este programa muestra mi comida favorito mi lugar favorito y mi cantante favorito

--- prueba1.java
+++ prueba1.java
@@ -2,9 +2,9 @@ public class prueba1{
 
 	public static void main(String args[]) {
 
-		System.out.println("Valbuena García");
 		System.out.println("Nací en 2004 y tengo 19 años");
 		System.out.println("Mi comida favorita es el arroz con pollo");
+		System.out.println("Mi cantante favorito es bad bunny");
 	} 
 
 }
